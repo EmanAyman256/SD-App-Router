e1b29a3d67c7743f9a22755fb78f7e28ea95c36b
EmanAyman256 <emanayman256@gmail.com>
2025-01-19 18:29:03 +0200

    update-baseUrl

--- src/app/shared/ApiService.service.ts
+++ src/app/shared/ApiService.service.ts
@@ -15,12 +15,12 @@ export class ApiService {
  
   // private baseUrl = "https://express-proxy-app.cfapps.us10-001.hana.ondemand.com/api"
   // private baseUrl = "https://proxy-app.cfapps.us10-001.hana.ondemand.com/api"
-  private baseUrl="/btpsd"
+  private baseUrl="/api"
   // private baseUrl = environment.production
   //   ? environment.apiUrl // Production URL
   //   : '/api';  // Development URL
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   get<T>(url: string, queryParam?: string, headers?: HttpHeaders): Observable<T> {
     let params = new HttpParams();
@@ -37,7 +37,7 @@ export class ApiService {
 
   getID<T>(url: string, id: number, params?: HttpParams, headers?: HttpHeaders): Observable<T> {
     //const options = { params, headers };
-
+    
     //headers =new HttpHeaders().set('Authorization',`Bearer ${ localStorage.getItem('token')}`)
     console.log(this.http.get<T>(`${this.baseUrl}/${url}/${id}`));
     return this.http.get<T>(`${this.baseUrl}/${url}/${id}`);
